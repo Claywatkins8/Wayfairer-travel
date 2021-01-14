22bf352ecff6aecc87a4011b13a83452bcfa8177
georgehu <georgehu865@gmail.com>
2021-01-14 10:58:32 -0700

    adds login and setup

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+# Once we have routes that need locking we can put in @login_required 
+
 # Define the home view
 
 
@@ -10,3 +15,18 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            # We are going to return redirect to home but will change this later
+            return redirect('')
+        else:
+            error_message = 'Invalid signup, please try again!'
+    form = UserCreationForm()
+    context = {'form':form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
