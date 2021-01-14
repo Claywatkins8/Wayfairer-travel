c0284c139bdc31df012e2d060a889cce8aaddbb9
qtpham <quincytpham@gmail.com>
2021-01-14 12:21:40 -0800

    Modals only load singup on home page and only login on accounts/login page need to figure out context for same page

--- main_app/views.py
+++ main_app/views.py
@@ -10,13 +10,6 @@ from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-    return render(request, 'home.html')
-
-
-def about(request):
-    return render(request, 'about.html')
-
-def signup(request):
     error_message = ''
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -29,4 +22,26 @@ def signup(request):
             error_message = 'Invalid signup, please try again!'
     form = UserCreationForm()
     context = {'form':form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
+    return render(request, 'home.html', context)
+
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+# REVIEW: Do not need page anymore? in modal?
+# def signup(request):
+#     error_message = ''
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             # We are going to return redirect to home but will change this later
+#             return redirect('')
+#         else:
+#             error_message = 'Invalid signup, please try again!'
+#     form = UserCreationForm()
+#     context = {'form':form, 'error_message': error_message}
+#     return render(request, 'registration/signup.html', context)
