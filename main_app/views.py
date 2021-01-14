429afc1c9cb612c4a5489f628acb9e7e85d444fb
claywatkins8 <claywatkins8@gmail.com>
2021-01-14 14:51:14 -0800

    signup form and login form created and linked to DB

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
-# Once we have routes that need locking we can put in @login_required 
+# Once we have routes that need locking we can put in @login_required
 
 # Define the home view
 
@@ -12,18 +12,19 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     error_message = ''
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
+        signup_form = UserCreationForm(request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
             login(request, user)
             # We are going to return redirect to home but will change this later
-            return redirect('')
+            return redirect('about/')
         else:
             error_message = 'Invalid signup, please try again!'
-    form = UserCreationForm()
-    context = {'form':form, 'error_message': error_message}
+    signup_form = UserCreationForm()
+    login_form = AuthenticationForm()
+    context = {'signup_form': signup_form,
+               'error_message': error_message, 'login_form': login_form}
     return render(request, 'home.html', context)
-
 
 
 def about(request):
