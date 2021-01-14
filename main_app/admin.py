87d76fcfc45b96dfd38eeb06a92f0887a5eadb59
georgehu <georgehu865@gmail.com>
2021-01-14 12:59:52 -0700

    created city model and extended the User model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, City
 
 # Register your models here.
+
+admin.site.register(User, UserAdmin)
+
+admin.site.register(City)
+
+
+
