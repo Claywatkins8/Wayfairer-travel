6ae6c25cf36f142b896a05944169182f0442557b
qtpham <quincytpham@gmail.com>
2021-01-22 11:22:55 -0800

    Removed commenting/cleaned code on base/home/urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
      path('', views.home, name='home'),
      path('about/', views.about, name='about'),
      path('accounts/', include('django.contrib.auth.urls')),
-     # path('accounts/signup', views.signup, name='signup'),
      path('profile/', views.profile, name='profile'),
      path('profile/edit/', views.profile_edit, name='profile_edit'),
      path('post/<int:post_id>/show/', views.post_show, name='post_show'),
