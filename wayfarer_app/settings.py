6e9aaa2eb4b619b3d33387b88c12e67ad2fe6a96
qtpham <quincytpham@gmail.com>
2021-01-22 09:25:44 -0800

    Attempting to refix email

--- wayfarer_app/settings.py
+++ wayfarer_app/settings.py
@@ -136,5 +136,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'latsin.dev@gmail.com'
-with open('main_app/static/passwords/email.txt') as f:
-    EMAIL_HOST_PASSWORD = f.read().strip()
+EMAIL_HOST_PASSWORD
